a809bcdcc62d8569853d50490a0e8756c83fb80b
Will <39478251+VITALISED@users.noreply.github.com>
2025-04-16 22:12:24 +1000

    mat_disable_bloom doesn't appear to exist at whenever, cba

--- primedev/client/clientcvarpatches.cpp
+++ primedev/client/clientcvarpatches.cpp
@@ -2,14 +2,9 @@
 
 ON_DLL_LOAD_CLIENT_RELIESON("client.dll", ClientCVARPatches, ConVar, (CModule module))
 {
-	// change default values of openinvite filter cvars to be archived
-
 	ConVar* Cvar_openInvites_filterByLanguage = g_pCVar->FindVar("openInvites_filterByLanguage");
 	Cvar_openInvites_filterByLanguage->AddFlags(FCVAR_ARCHIVE_PLAYERPROFILE);
 
 	ConVar* Cvar_openInvites_filterByRegion = g_pCVar->FindVar("openInvites_filterByRegion");
 	Cvar_openInvites_filterByRegion->AddFlags(FCVAR_ARCHIVE_PLAYERPROFILE);
-
-	ConVar* Cvar_mat_disable_bloom = g_pCVar->FindVar("mat_disable_bloom");
-	Cvar_mat_disable_bloom->AddFlags(FCVAR_ARCHIVE_PLAYERPROFILE);
 }
