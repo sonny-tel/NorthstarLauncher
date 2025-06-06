a677ab1e78abb0685f8df6912df5f0b489beef06
Will <39478251+VITALISED@users.noreply.github.com>
2025-06-07 06:43:59 +1000

    handle this slightly better

--- primedev/scripts/scriptremotes.cpp
+++ primedev/scripts/scriptremotes.cpp
@@ -5,9 +5,18 @@ REPLACE_SQFUNC(Remote_RegisterFunction, ScriptContext::CLIENT)
 {
 	SQStackInfos si;
 
+	if( !g_pSquirrel<context>->m_pSQVM || !g_pSquirrel<context>->m_pSQVM->sqvm )
+		return SQRESULT_NULL;
+
 	if( g_pVanillaCompatibility->GetVanillaCompatibility() )
 	{
 		g_pSquirrel<context>->sq_stackinfos(sqvm, 1, si);
+
+		if( si._name == nullptr || si._sourceName == nullptr )
+		{
+			g_pSquirrel<context>->raiseerror(sqvm, "No source name or function name found in stack infos. Can't register remote.");
+			return SQRESULT_ERROR;
+		}
 
 		// honestly, it would be better to try and figure out how you get disconnected for being out of sync
 		if( strcmp(si._name, "RemoteFunctions_Init") == 0
