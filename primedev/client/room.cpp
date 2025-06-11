3a99e8c50caecf8256b69f3111a37ff66e4e9d37
Will <39478251+VITALISED@users.noreply.github.com>
2025-06-11 11:41:17 +1000

    initial working room crap

--- primedev/client/room.cpp
+++ primedev/client/room.cpp
@@ -1,9 +1,11 @@
 #include "room.h"
 
-typedef void* (*JoinPlayerGameRoom_t)(const char* uid);
+typedef void* (*JoinPlayerGameRoom_t)(const char* roomId);
 JoinPlayerGameRoom_t JoinPlayerGameRoom;
+
 char* room1;
 char* room2;
+char* room3;
 
 void ConCommand_ns_join_room(const CCommand& args)
 {
@@ -12,29 +14,32 @@ void ConCommand_ns_join_room(const CCommand& args)
     JoinPlayerGameRoom(roomId);
 }
 
-void ConCommand_ns_print_room(const CCommand& args)
+void ConCommand_ns_dump_room(const CCommand& args)
 {
 	NOTE_UNUSED(args);
 
 	if (room1)
-	{
-		// not it
-		//spdlog::info("Origin Player Room: {}", room1);
-	}
+		spdlog::info("room related offset: {}", room1);
+
 	if (room2)
-	{
-		spdlog::info("Community Room: {}", room2);
-	}
+		spdlog::info("community room: {}", room2);
+
+	if (room3)
+		spdlog::info("inviteRoom: {}", room3);
+
+	const char* match_partySub = g_pCVar->FindVar("match_partySub")->GetString();
+
+	if (match_partySub)
+		spdlog::info("match_partySub: {}", match_partySub);
 }
 
 ON_DLL_LOAD_CLIENT_RELIESON("engine.dll", PartyRoom, ConCommand, (CModule module))
 {
     JoinPlayerGameRoom = module.Offset(0x187C70).RCast<JoinPlayerGameRoom_t>();
-	RegisterConCommand("ns_join_room", ConCommand_ns_join_room, "Join a server by its room ID", FCVAR_CLIENTDLL);
+	RegisterConCommand("ns_join_room", ConCommand_ns_join_room, "Join a server by its room ID (value of match_partySub)", FCVAR_CLIENTDLL);
 
 	room1 = module.Offset(0x1314E538).RCast<char*>();
 	room2 = module.Offset(0x13F8E2D0).RCast<char*>();
-	RegisterConCommand("ns_print_room", ConCommand_ns_print_room, "Dump room info", FCVAR_CLIENTDLL);
-
-	// module.Offset(0x8c3e9).Patch("0x09");
+	room3 = module.Offset(0x1314B1CC).RCast<char*>();
+	RegisterConCommand("ns_dump_room", ConCommand_ns_dump_room, "Dump room info", FCVAR_CLIENTDLL);
 }
