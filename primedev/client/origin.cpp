c6c47c5a5c1fca7bc6ce448efe8b40f6d4ef4ae6
Allusive <154700875+AllusiveWheat@users.noreply.github.com>
2025-07-01 11:16:39 -0700

    origin stuff

--- primedev/client/origin.cpp
+++ primedev/client/origin.cpp
@@ -2,6 +2,8 @@
 #include "r2client.h"
 
 OriginRequestAuthCodeType OriginRequestAuthCode;
+OriginGetPresenceType OriginGetPresence;
+
 std::string* tmpTok = nullptr;
 
 void OriginAuthcodeStrcpyCallback(__int64 a1, __int64* a2)
@@ -33,7 +35,7 @@ std::string* GetNewOriginToken(int timeoutSeconds)
 
 	if (userId > 0)
 		res = OriginRequestAuthCode(userId, "TITANFALL2-PC-SERVER", OriginAuthcodeStrcpyCallback, 0, 30000, 0);
-
+	
 	auto start = std::chrono::steady_clock::now();
 
 	for(;;)
@@ -80,9 +82,68 @@ std::string* GetNewOriginToken(int timeoutSeconds)
 	return tok;
 }
 
-ON_DLL_LOAD("OriginSDK.dll", OriginSDK, (CModule module))
+std::string PresenceToString(OriginPresenceEnum presence)
+{
+	switch (presence)
+	{
+	case OriginPresenceEnum::IS_ONLINE:
+		return "Online";
+	case OriginPresenceEnum::IS_OFFLINE:
+		return "Offline";
+	case OriginPresenceEnum::AWAY:
+		return "Away";
+	case OriginPresenceEnum::IS_IN_PARTY:
+		return "In Party";
+	case OriginPresenceEnum::BUSY:
+		return "Busy";
+	case OriginPresenceEnum::IN_GAME:
+		return "In Game";
+	case OriginPresenceEnum::IS_IN_GAME_PARTY:
+		return "In Game Party";
+	default:
+		return "Unknown";
+	}
+}
+
+ADD_SQFUNC("PresenceData", NSGetOriginPresence, "string uid", "", ScriptContext::UI)
+{
+
+	std::string uid = g_pSquirrel<context>->getstring(sqvm, 1);
+	__int64 userId = _strtoi64(uid.c_str(), nullptr, 10);
+	OriginPresenceEnum presence;
+	char* sessionId = new char[256];
+	OriginGetPresence(userId, &presence, nullptr, 0, nullptr, 0, sessionId, 256);
+	spdlog::info("Origin presence for {}: {},{}", uid, PresenceToString(presence), sessionId);
+	g_pSquirrel<context>->pushnewstructinstance(sqvm, 2);
+	g_pSquirrel<context>->pushinteger(sqvm, static_cast<int>(presence));
+	g_pSquirrel<context>->sealstructslot(sqvm, 0);
+
+	g_pSquirrel<context>->pushstring(sqvm, sessionId,-1);
+	g_pSquirrel<context>->sealstructslot(sqvm, 1);
+
+	return SQRESULT_NOTNULL;
+
+}
+
+void ConCommand_fish_origin(const CCommand& args)
+{
+	__int64 userId = 0;
+	if (g_pLocalPlayerUserID)
+		userId = _strtoi64(g_pLocalPlayerUserID, nullptr, 10);
+	OriginPresenceEnum presence;
+	char* sessionId = new char[256];
+	OriginGetPresence(userId, &presence, nullptr, 0, nullptr,0, sessionId, 256);
+	spdlog::info("Origin presence: {},{}", PresenceToString(presence), sessionId);
+}
+ON_DLL_LOAD_CLIENT_RELIESON("engine.dll", ClientOrigin, ConCommand, (CModule module))
+{
+	RegisterConCommand("ns_fish", ConCommand_fish_origin, "does stuff idk", 0);
+}
+
+ON_DLL_LOAD("OriginSDK.dll", OriginSDK,(CModule module))
 {
 	// takes 5 params: user_id (g_pLocalPlayerUserID), the game ("TITANFALL2-PC-SERVER"), callback func to strcpy the token to which looks like void(*)(__int64 a1, char* a2),
 	// and 3 ints which idk what they are but are 0, 30000 and 0 by default. probably some token ttl stuff
 	OriginRequestAuthCode = module.GetExportedFunction("OriginRequestAuthCode").RCast<OriginRequestAuthCodeType>();
+	OriginGetPresence = module.GetExportedFunction("OriginGetPresence").RCast<OriginGetPresenceType>();
 }
--- primedev/client/origin.h
+++ primedev/client/origin.h
@@ -3,5 +3,20 @@
 typedef void (*OriginAuthcodeStrcpyCallbackType)(__int64 a1, __int64* a2);
 typedef __int64 (*OriginRequestAuthCodeType)(__int64 userId, const char* game, OriginAuthcodeStrcpyCallbackType callback, __int64* a4, int a5, __int64* a6);
 extern OriginRequestAuthCodeType OriginRequestAuthCode;
+enum OriginPresenceEnum
+{
+	UNK,
+	IS_OFFLINE,
+	IS_ONLINE,
+	IN_GAME,
+	BUSY,
+	AWAY,
+	IS_IN_PARTY,
+	IS_IN_GAME_PARTY,
+	IS_INVITE_ONLY
+};
+typedef void (*OriginGetPresenceType)(
+	__int64 userId, OriginPresenceEnum* presenceData, char* a3, __int64 a4, char* a5, __int64 a6, char* a7, __int64 a8);
 
+extern OriginGetPresenceType OriginGetPresence;
 std::string* GetNewOriginToken(int timeoutSeconds);
