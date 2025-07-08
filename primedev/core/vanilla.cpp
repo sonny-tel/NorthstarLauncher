ed43b2b3a20f77ccf51772d92e51db245bd1b4a8
Will <39478251+VITALISED@users.noreply.github.com>
2025-07-08 17:13:04 +1000

    more vanilla checks

--- primedev/core/vanilla.cpp
+++ primedev/core/vanilla.cpp
@@ -15,6 +15,24 @@ AUTOHOOK(matchmake, engine.dll + 0xF220, int*, __fastcall, ())
 	return matchmake();
 }
 
+// clang-format off
+AUTOHOOK(connectwithtoken, engine.dll + 0x769C0, int*, __fastcall, (__int64 a1))
+// clang-format on
+{
+	g_pVanillaCompatibility->SetCompatabilityMode(VanillaCompatibility::CompatibilityMode::Vanilla);
+
+	return connectwithtoken(a1);
+}
+
+// clang-format off
+AUTOHOOK(silentconnect, engine.dll + 0x76F00, int*, __fastcall, (__int64 a1))
+// clang-format on
+{
+	g_pVanillaCompatibility->SetCompatabilityMode(VanillaCompatibility::CompatibilityMode::Vanilla);
+
+	return silentconnect(a1);
+}
+
 // vanilla only uses connectwithtoken and silentconnect, this is actually pretty good for us to differentiate
 // clang-format off
 AUTOHOOK(concommand_connect, engine.dll + 0x76720, __int64, __fastcall, (__int64 a1))
