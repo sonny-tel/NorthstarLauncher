265a40d3a11d9c0226381c2009f578e5d0d96dfd
Will <39478251+VITALISED@users.noreply.github.com>
2025-07-11 00:46:50 +1000

    comment out OriginReadEnumerationSyncHook

--- primedev/client/origin.cpp
+++ primedev/client/origin.cpp
@@ -156,13 +156,13 @@ ON_DLL_LOAD_CLIENT_RELIESON("engine.dll", ClientOrigin, ConCommand, (CModule mod
 
 }
 
-static int OriginReadEnumerationSyncHook(__int64 a1, __int64 a2, __int64 a3, __int64 a4, __int64 a5, __int64 a6) {
-	// print all the parameters in hex format
+// static int OriginReadEnumerationSyncHook(__int64 a1, __int64 a2, __int64 a3, __int64 a4, __int64 a5, __int64 a6) {
+// 	// print all the parameters in hex format
 
 
-	spdlog::info("OriginReadEnumerationSyncHook called with a1: {}, a2: {}, a3: {}, a4: {}, a5: {}, a6: {}", a1, a2, a3, a4, a5,a6);
-	return OriginReadEnumerationSync(a1, a2, a3, a4, a5,a6);
-}
+// 	spdlog::info("OriginReadEnumerationSyncHook called with a1: {}, a2: {}, a3: {}, a4: {}, a5: {}, a6: {}", a1, a2, a3, a4, a5,a6);
+// 	return OriginReadEnumerationSync(a1, a2, a3, a4, a5,a6);
+// }
 
 ON_DLL_LOAD("OriginSDK.dll", OriginSDK,(CModule module))
 {
