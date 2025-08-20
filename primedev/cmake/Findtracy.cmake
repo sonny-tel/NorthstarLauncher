fff4c16495b180038d263c280686ac90ca7c18e8
Allusive <154700875+AllusiveWheat@users.noreply.github.com>
2025-08-20 15:22:19 -0700

    kill tracy

--- primedev/cmake/Findtracy.cmake
+++
@@ -1,6 +0,0 @@
-if(NOT tracy_FOUND)
-    check_init_submodule(${PROJECT_SOURCE_DIR}/primedev/thirdparty/tracy)
-
-	add_subdirectory(${PROJECT_SOURCE_DIR}/primedev/thirdparty/tracy tracy)
-    set(tracy_FOUND 1)
-endif()
