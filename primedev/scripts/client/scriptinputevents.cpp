3b36d1f57ec35695859eaba2f42a992a320c6f11
Will <39478251+VITALISED@users.noreply.github.com>
2025-10-30 20:03:20 +1100

    use asynccall instead whoops

--- primedev/scripts/client/scriptinputevents.cpp
+++ primedev/scripts/client/scriptinputevents.cpp
@@ -11,11 +11,11 @@ ConVar* Cvar_cl_dump_input_events;
 AUTOHOOK(CInputSystem__PostEvent, inputsystem.dll + 0x7EC0, void, __fastcall, (void* self, int nType, int nTick, int nData, int nData2, int nData3))
 // clang-format on
 {
+	CInputSystem__PostEvent(self, nType, nTick, nData, nData2, nData3);
+
 	if (Cvar_cl_dump_input_events->GetBool())
 		spdlog::info("CInputSystem::PostEvent - Type: {}, Tick: {}, Data: {}, Data2: {}, Data3: {}",
 			nType, nTick, nData, nData2, nData3);
-
-	HSQUIRRELVM cl_sqvm = g_pSquirrel<ScriptContext::CLIENT>->m_pSQVM ? g_pSquirrel<ScriptContext::CLIENT>->m_pSQVM->sqvm : nullptr;
 
 	if( g_pSquirrel<ScriptContext::CLIENT>->m_pSQVM && g_pSquirrel<ScriptContext::CLIENT>->m_pSQVM->sqvm )
 	{
@@ -23,7 +23,7 @@ AUTOHOOK(CInputSystem__PostEvent, inputsystem.dll + 0x7EC0, void, __fastcall, (v
 		SQObject functionobj {};
 
 		if( g_pSquirrel<ScriptContext::CLIENT>->sq_getfunction(sqvm, CInputSystem__PostEvent_SQFunc, &functionobj, 0) == 0)
-			g_pSquirrel<ScriptContext::CLIENT>->Call(
+			g_pSquirrel<ScriptContext::CLIENT>->AsyncCall(
 				CInputSystem__PostEvent_SQFunc, nType, nTick, nData, nData2, nData3);
 	}
 
@@ -33,12 +33,9 @@ AUTOHOOK(CInputSystem__PostEvent, inputsystem.dll + 0x7EC0, void, __fastcall, (v
 		SQObject functionobj {};
 
 		if( g_pSquirrel<ScriptContext::UI>->sq_getfunction(sqvm, CInputSystem__PostEvent_SQFunc, &functionobj, 0) == 0)
-			g_pSquirrel<ScriptContext::UI>->Call(
+			g_pSquirrel<ScriptContext::UI>->AsyncCall(
 				CInputSystem__PostEvent_SQFunc, nType, nTick, nData, nData2, nData3);
 	}
-
-	CInputSystem__PostEvent(self, nType, nTick, nData, nData2, nData3);
-	return;
 }
 
 ON_DLL_LOAD_RELIESON("inputsystem.dll", FastCallbacks, ConVar, (CModule module))
