6787270e83aa1a0694be57ca894f866c31435ce2
Will <39478251+VITALISED@users.noreply.github.com>
2025-10-30 15:37:29 +1100

    Add custom input event handling

---
+++ primedev/inputsystem/inputevents.cpp
@@ -0,0 +1,38 @@
+#include "inputenums.h"
+#include "buttoncode.h"
+#include "core/convar/convar.h"
+#include "squirrel/squirrel.h"
+
+AUTOHOOK_INIT()
+
+ConVar* Cvar_cl_dump_input_events;
+
+// clang-format off
+AUTOHOOK(CInputSystem__PostEvent, inputsystem.dll + 0x7EC0, void, __fastcall, (void* self, int nType, int nTick, int nData, int nData2, int nData3))
+// clang-format on
+{
+	if (Cvar_cl_dump_input_events->GetBool())
+		spdlog::info("CInputSystem::PostEvent - Type: {}, Tick: {}, Data: {}, Data2: {}, Data3: {}",
+			nType, nTick, nData, nData2, nData3);
+
+	if( g_pSquirrel<ScriptContext::CLIENT>->m_pSQVM && g_pSquirrel<ScriptContext::CLIENT>->m_pSQVM->sqvm )
+		g_pSquirrel<ScriptContext::CLIENT>->Call(
+			"CInputSystem_ProcessPostEvent", nType, nTick, nData, nData2, nData3);
+	if( g_pSquirrel<ScriptContext::UI>->m_pSQVM && g_pSquirrel<ScriptContext::UI>->m_pSQVM->sqvm )
+		g_pSquirrel<ScriptContext::UI>->Call(
+			"CInputSystem_ProcessPostEvent", nType, nTick, nData, nData2, nData3);
+
+	CInputSystem__PostEvent(self, nType, nTick, nData, nData2, nData3);
+	return;
+}
+
+ON_DLL_LOAD_RELIESON("inputsystem.dll", FastCallbacks, ConVar, (CModule module))
+{
+	Cvar_cl_dump_input_events = new ConVar(
+		"cl_dump_input_events",
+		"0",
+		FCVAR_NONE,
+		"Dump input events to console for debugging purposes.");
+
+	AUTOHOOK_DISPATCH();
+}
--- primedev/scripts/client/scriptpresence.cpp
+++ primedev/scripts/client/scriptpresence.cpp
@@ -1,34 +1,6 @@
 #include "squirrel/squirrel.h"
 #include "client/origin.h"
 
-// void DecodeJsonTable(HSQUIRRELVM sqvm, rapidjson::GenericValue<rapidjson::UTF8<char>, rapidjson::MemoryPoolAllocator<SourceAllocator>>*
-// obj)
-//{
-//	g_pSquirrel<context>->newtable(sqvm);
-//
-//	for (auto itr = obj->MemberBegin(); itr != obj->MemberEnd(); itr++)
-//	{
-//		switch (itr->value.GetType())
-//		{
-//		case rapidjson::kObjectType:
-//			g_pSquirrel<context>->pushstring(sqvm, itr->name.GetString(), -1);
-//			DecodeJsonTable<context>(
-//				sqvm, (rapidjson::GenericValue<rapidjson::UTF8<char>, rapidjson::MemoryPoolAllocator<SourceAllocator>>*)&itr->value);
-//			g_pSquirrel<context>->newslot(sqvm, -3, false);
-//			break;
-//		case rapidjson::kArrayType:
-//			g_pSquirrel<context>->pushstring(sqvm, itr->name.GetString(), -1);
-//			DecodeJsonArray<context>(
-//				sqvm, (rapidjson::GenericValue<rapidjson::UTF8<char>, rapidjson::MemoryPoolAllocator<SourceAllocator>>*)&itr->value);
-//			g_pSquirrel<context>->newslot(sqvm, -3, false);
-//			break;
-//		case rapidjson::kStringType:
-//			g_pSquirrel<context>->pushstring(sqvm, itr->name.GetString(), -1);
-//			g_pSquirrel<context>->pushstring(sqvm, itr->value.GetString(), -1);
-//
-//			g_pSquirrel<context>->newslot(sqvm, -3, false);
-//
-
 ADD_SQFUNC("table< string, string >", NSGetFriendSubscriptionMap, "", "", ScriptContext::UI)
 {
 	g_pSquirrel<context>->newtable(sqvm);
