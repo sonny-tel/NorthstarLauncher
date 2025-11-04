1c2141d43b80ff82e546134301ea237aaaa67fa3
Will <39478251+VITALISED@users.noreply.github.com>
2025-11-05 00:00:51 +1100

    forward declare INetMessage

--- primedev/engine/netchannel.h
+++ primedev/engine/netchannel.h
@@ -1,6 +1,7 @@
 #pragma once
 
 class CNetChan;
+class INetMessage;
 
 typedef bool (__fastcall* CNetChan__RegisterMessage_t)(CNetChan* thisptr, INetMessage* msg);
 extern CNetChan__RegisterMessage_t CNetChan__RegisterMessage;
