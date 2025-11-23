fbeb2e94ac1a917eaeb8bec9f7f914c812fe2172
Will <39478251+VITALISED@users.noreply.github.com>
2025-11-23 16:24:13 +1100

    also this

--- primedev/shared/playlist.cpp
+++ primedev/shared/playlist.cpp
@@ -120,7 +120,4 @@ ON_DLL_LOAD_RELIESON("engine.dll", PlaylistHooks, (ConCommand, ConVar), (CModule
 	// patch to prevent clc_SetPlaylistVarOverride from being able to crash servers if we reach max overrides due to a call to Error (why is
 	// this possible respawn, wtf) todo: add a warning for this
 	module.Offset(0x18ED8D).Patch("C3");
-
-	// patch to allow setplaylistvaroverride to be called before map init on dedicated and private match launched through the game
-	// only run on non-vanilla as this patch makes users unable to change private match settings on vanilla
 }
