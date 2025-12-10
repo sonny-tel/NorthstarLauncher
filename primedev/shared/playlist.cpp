fdfa5d632dcc72be5069948cfcecfd8fb7872163
Will <39478251+VITALISED@users.noreply.github.com>
2025-12-11 00:28:15 +1100

    add this byte patch back for dedi

--- primedev/shared/playlist.cpp
+++ primedev/shared/playlist.cpp
@@ -6,6 +6,7 @@
 #include "engine/hoststate.h"
 #include "engine/r2engine.h"
 #include "server/serverpresence.h"
+#include "dedicated/dedicated.h"
 
 AUTOHOOK_INIT()
 
@@ -122,4 +123,7 @@ ON_DLL_LOAD_RELIESON("engine.dll", PlaylistHooks, (ConCommand, ConVar), (CModule
 	// patch to prevent clc_SetPlaylistVarOverride from being able to crash servers if we reach max overrides due to a call to Error (why is
 	// this possible respawn, wtf) todo: add a warning for this
 	module.Offset(0x18ED8D).Patch("C3");
+
+	if( IsDedicatedServer() )
+		module.Offset(0x18ED17).NOP(6);
 }
