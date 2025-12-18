f42d98e42e0b0e16147b87b0396666cd1dc25f2d
Will <39478251+VITALISED@users.noreply.github.com>
2025-12-19 09:31:11 +1100

    script stuff

--- primedev/scripts/client/scriptgetserverinfo.cpp
+++ primedev/scripts/client/scriptgetserverinfo.cpp
@@ -2,12 +2,12 @@
 #include "engine/r2engine.h"
 #include "engine/netmessages.h"
 
-ADD_SQFUNC("void", NSRequestServerInfo, "string ipv4, int port, bool requestMods", "", ScriptContext::UI)
+ADD_SQFUNC("void", NSRequestServerInfo, "string ip, int port, bool requestMods", "", ScriptContext::UI)
 {
 	const SQChar* ip = g_pSquirrel<context>->getstring(sqvm, 1);
 	SQInteger port = g_pSquirrel<context>->getinteger(sqvm, 2);
 	bool requestMods = g_pSquirrel<context>->getbool(sqvm, 3);
-	std::string address = fmt::format("[::ffff:{}]:{}", ip, port);
+	std::string address = fmt::format("[{}]:{}", ip, port);
 	netadr_t addr = CNetAdr(address.c_str());
 
 	char buffer[64];
