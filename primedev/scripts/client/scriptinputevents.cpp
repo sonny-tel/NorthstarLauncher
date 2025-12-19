29a4ac7f159e22ba67a4f3b845fe0b3387b52fab
Will <39478251+VITALISED@users.noreply.github.com>
2025-12-20 05:48:59 +1100

    get rid of print might be causing problems idk

--- primedev/scripts/client/scriptinputevents.cpp
+++ primedev/scripts/client/scriptinputevents.cpp
@@ -15,29 +15,16 @@ AUTOHOOK_INIT()
 				CInputSystem__PostEvent_SQFunc, nType, nTick, nData, nData2, nData3); \
 	}
 
-ConVar* Cvar_cl_dump_input_events;
-
 // clang-format off
 AUTOHOOK(CInputSystem__PostEvent, inputsystem.dll + 0x7EC0, void, __fastcall, (void* self, int nType, int nTick, int nData, int nData2, int nData3))
 // clang-format on
 {
 	CInputSystem__PostEvent(self, nType, nTick, nData, nData2, nData3);
-
-	if (Cvar_cl_dump_input_events->GetBool())
-		spdlog::info("CInputSystem::PostEvent - Type: {}, Tick: {}, Data: {}, Data2: {}, Data3: {}",
-			nType, nTick, nData, nData2, nData3);
-
 	CALL_INPUTSYS_SQ_FUNC(CLIENT);
 	CALL_INPUTSYS_SQ_FUNC(UI);
 }
 
 ON_DLL_LOAD_RELIESON("inputsystem.dll", FastCallbacks, ConVar, (CModule module))
 {
-	Cvar_cl_dump_input_events = new ConVar(
-		"cl_dump_input_events",
-		"0",
-		FCVAR_NONE,
-		"Dump input events to console for debugging purposes.");
-
 	AUTOHOOK_DISPATCH();
 }
